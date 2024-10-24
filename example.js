76f524f571353a9224d8d7b44532358fd0a1adaa
briannaordaz <briannaordaz5@gmail.com>
2024-10-23 20:18:25 -0700

    first commit

---
+++ example.js
@@ -0,0 +1,20 @@
+// Lecture 2-2
+// Show assigning strings, Booleans, numbers to variable.
+// Show a  constant variable assignment. 
+// Show what happens when you try to override it.
+//Show calling console.log() and Math.min(), and Math.max().
+
+// string variable
+let myVariable = "Hello World!"
+console.log(myVariable);
+
+// change the string variable value
+myVariable = "I changed the value of the string variable here.";
+console.log(myVariable);
+
+// const variable
+const pi = 3.14;
+// pi = 2; // throws an error
+
+console.log(Math. min(pi, 2 ));
+
---
+++ script2.js
@@ -0,0 +1,13 @@
+//string variable
+let myCoding = "Hello!!"
+console.log(myCoding);
+
+//changed variable
+myCoding = "Hey!!";
+console.log(myCoding);
+
+//const variable
+const pi = 3.14;
+
+let myNumber = 1515;
+console.log(Math.min(myNumber, pi));
